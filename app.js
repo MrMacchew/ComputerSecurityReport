48706cd5754ff0af2b01423efe82d29b6d0fb7e0
MrMacchew <mattcain@weber.edu>
2016-12-16 16:21:11 -0700

    Added test JS files

--- app.js
+++ app.js
@@ -3,14 +3,17 @@
     name: 'Executioner01',
     IDF: true ,
     SCEP: true ,
-    lab: false
+    lab: false,
+    college: 'CTS'
   },
   {
     name: 'myVM1',
     IDF: false ,
     SCEP: false,
-    lab: true
+    lab: true,
+    college: "IT"
   }];
+
   var app = angular.module('computerReport', []);
   app.controller('ReportController', function(){
     this.computers = computers;
